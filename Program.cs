b698633506895c8cf8c2259a0526216b0beec54c
Yaroslav <y.synytsyn@gmail.com>
2021-03-26 08:18:28 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lycky_ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
